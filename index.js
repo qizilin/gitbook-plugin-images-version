93c580ce404500ca4f8be3377b9787cba0411a8a
qizilin <1019452788@qq.com>
2019-05-30 22:01:17 +0800

    base图片不需要加版本号

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ var URI = require('urijs')
 
 var version = Number.parseInt(Date.now() / 1000)
 module.exports = {
+  filters: {
+    addVersion: function(url) {
+      var url = new URI(url)
+      url.addQuery('_', version)
+      return url.toString()
+    }
+  },
   hooks: {
     page: function(page) {
       var $ = cheerio.load(page.content)
       $('img').each(function(index, img) {
         var src = img.attribs.src
-        if (src) {
+        if (src && src.indexOf('data:image') === -1) {
           var url = new URI(src)
-          url.addQuery('v', version)
+          url.addQuery('_', version)
           img.attribs.src = url.toString()
         }
       })
